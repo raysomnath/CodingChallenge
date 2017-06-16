1ac38b2a33fa24442a608912c5d172746764182e
raysomnath@hotmail.com <raysomnath@hotmail.com>
2017-06-16 20:12:45 +1000

    Initial Commit
    Initial Commit

---
+++ CodingChallenges/BinaryGap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingChallenges
+{
+    static class BinaryGap
+    {
+        public static int solution(int N)
+        {
+
+            if (N == 0 || N == 1)
+                return 0;
+
+            string NumberInBinaryFormat = Convert.ToString(N, 2);
+            List<int> BinaryGap = null;
+
+            if (NumberInBinaryFormat.IndexOf('0') == -1)
+                return 0;
+            else
+            {
+                BinaryGap = new List<int>();
+
+                while (NumberInBinaryFormat.Length > 1 && (NumberInBinaryFormat.IndexOf('0') > -1) && (NumberInBinaryFormat.IndexOf('1') > -1))
+                {
+                    if (NumberInBinaryFormat.IndexOf('0') > -1)
+                    {
+                        NumberInBinaryFormat = NumberInBinaryFormat.Substring(NumberInBinaryFormat.IndexOf('0'),
+                                                                              NumberInBinaryFormat.Length - NumberInBinaryFormat.IndexOf('0'));
+
+                        if (NumberInBinaryFormat.IndexOf('1') > -1)
+                        {
+                            var GapLength = NumberInBinaryFormat.IndexOf('1');
+
+                            if (!BinaryGap.Contains(GapLength))
+                            {
+                                BinaryGap.Add(GapLength);
+                            }
+                            NumberInBinaryFormat = NumberInBinaryFormat.Substring(NumberInBinaryFormat.IndexOf('1'),
+                                                                              NumberInBinaryFormat.Length - NumberInBinaryFormat.IndexOf('1'));
+                        }
+                        else
+                            break;
+                    }
+                    else
+                        break;
+                }
+
+                if (BinaryGap.Count > 0)
+                    return BinaryGap.Max();
+                else
+                    return 0;
+            }
+
+        }
+    }
+}
---
+++ CodingChallenges/Start.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            CheckArrayEquilibiriumProblem();
+        }
+
+        private static void CheckArrayEquilibiriumProblem()
+        {
+            int[] A = { -1, 3, -4, 5, 1, -6, 2, 1 };
+
+            Console.WriteLine(DateTime.Now.ToString());
+            var x = System.DateTime.Now;
+            var retVal = ArrayEqulibrium.solution(A);
+            Console.WriteLine(retVal);
+            Console.WriteLine((DateTime.Now - x).ToString());
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine(DateTime.Now.ToString());
+             x = System.DateTime.Now;
+             retVal = BinaryGap.solution(529);
+
+            Console.WriteLine("Binary gap for 529 is " + retVal);
+            Console.WriteLine("Binary gap for 20 is " + BinaryGap.solution(20));
+            Console.WriteLine("Binary gap for 15 is " + BinaryGap.solution(15));
+            Console.WriteLine("Binary gap for " + Int32.MaxValue + "is " + BinaryGap.solution(Int32.MaxValue));
+
+            Console.WriteLine((DateTime.Now - x).ToString());
+
+            Console.Read();
+        }
+    }
+}
